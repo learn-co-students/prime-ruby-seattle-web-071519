1670d7fc22c0bf5c845719d952323ee38df0c592
Ellyn <eka249@nyu.edu>
2019-07-14 14:44:26 -0700

    com

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+    arr=Array(2..integer-1)
+    if integer <=1
+        return false
+    elsif integer ==2
+        return true
+        #binding.pry
+    end
+    for i in arr
+        if (integer % i) ==0
+            return false
+        end
+    end
+    true
+end
+
+
